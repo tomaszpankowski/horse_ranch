210806e36e586a8e2401c7612f86f0104fc9874a
tomaszpankowski <pankowski.email@gmail.com>
2022-02-08 21:16:32 +0100

    layout done

--- src/views/tournament.js
+++ src/views/tournament.js
@@ -1,11 +1,18 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import TournamentS1 from "../components/tournament-s1";
+import TournamentS2 from "../components/tournament-s2";
+import TournamentS3 from "../components/tournament-s3";
+import TournamentS4 from "../components/tournament-s4";
 
 class Tournament extends Component{
     render(){
         return(        
-            <Container fluid className="tournament-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <TournamentS1/>
+                <TournamentS2/>
+                <TournamentS3/>
+                <TournamentS4/>
             </Container>    
         );
     }
